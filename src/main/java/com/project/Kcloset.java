270f93e1d0c15047a3894ec2daf3f290d20d8f28
SongYoungMin <dudals789@naver.com>
2026-06-14 14:35:33 +0900

    정리

---
+++ src/main/java/com/project/Kcloset.java
@@ -0,0 +1,66 @@
+package com.project;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Kcloset {
+
+    public static void main(String[] args) {
+
+        int[][] points1 = {
+                {1, 3},
+                {-2, 2}
+        };
+        int k1 = 1;
+
+        printResult(kClosest(points1, k1));
+
+
+        int[][] points2 = {
+                {3, 3},
+                {5, -1},
+                {-2, 4}
+        };
+        int k2 = 2;
+
+        printResult(kClosest(points2, k2));
+    }
+
+    // {3, 3} = 18
+    // {5, -1} = 26
+    // {-2, 4} = 20
+
+    // 우선수위 큐,
+
+    public static int[][] kClosest(int[][] points, int k) {
+
+        // TODO
+        PriorityQueue<int []> priorityQueue = new PriorityQueue<>((a, b) -> distance(a) - distance(b));
+
+        int [][] result =new int[k][2];
+
+        for(int [] data : points){
+            priorityQueue.add(data);
+        }
+
+        for(int i = 0; i < k; i++){
+            int[] poll = priorityQueue.poll();
+
+            result[i] = poll;
+        }
+
+        return result;
+    }
+
+    public static int distance(int[] data){
+        return data[0] * data[0] + data[1] * data[1];
+    }
+
+
+    private static void printResult(int[][] result) {
+        for (int[] point : result) {
+            System.out.println(Arrays.toString(point));
+        }
+        System.out.println("------");
+    }
+}
